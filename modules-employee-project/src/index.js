6632c73307753abbe5102257d9a4368dac0f69ef
tacettinkustu <tacettinkustu>
2020-09-19 13:23:31 +0300

    creating requests

--- modules-employee-project/src/index.js
+++ modules-employee-project/src/index.js
@@ -1,1 +1,15 @@
-console.log("Test");
+import { Request } from "./request";
+
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const request = new Request("http://localhost:3000/employees");
+
+request
+  .delete(9)
+  .then((employee) => console.log(employee))
+  .catch((err) => console.log(err));
